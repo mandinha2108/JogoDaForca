2fa039b7f8c3f5b60576a643b73c07f368e2c735
mandinha2108] <amafur8@gmail.com>
2024-10-10 12:25:17 -0300

    iniciado

---
+++ main.js
@@ -0,0 +1,5 @@
+const palavras = ["programacao","javascript","bootstrap"];
+let tentativasUsadas = 0;
+let letras = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+let palavraSecreta, palavraOculta;
+let jogando;
